a0ddbede48cc4436c987e7c854931b3b168ffd4b
Andy <anandit46@gmail.com>
2020-04-03 15:52:57 -0700

    Add project files.

---
+++ Controllers/GigsController.cs
@@ -0,0 +1,24 @@
+﻿using GigHub.Models;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private ApplicationDbContext DB;
+        public GigsController()
+        {
+            DB = new ApplicationDbContext();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+    }
+}
